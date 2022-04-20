e5858860c7d9aae89cb44f28bc38f92622418555
Samuel <sbabinski@outlook.com>
2022-04-20 17:00:33 -0300

    Add listeners for the lock-in buttons
    Create lock-in function to pass playerFactory the players attributes.
    Still need to implement symbol lock.

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-let playerFactory = (name, type, symbol, color) => {
-    const name = name;
-    const type = type;
-    const symbol = symbol;
-    const color = color;
-    const wins = 0;
+const playerFactory = (name, type, symbol, color) => {
+    let wins = 0;
+    const countWins = () => wins++;
+    const getPlyerName = () => name;
+    const getPlyerType = () => type;
+    const getPlyerSymbol = () => symbol;
+    const getPlyerColor = () => color;
+    const getPlyerWins = () => wins;
 
-    const countWins = () => {
-        this.wins++;
-    }
-
-    const getPlyerName = () => this.name;
-    const getPlyerType = () => this.type;
-    const getPlyerSymbol = () => this.symbol;
-    const getPlyerColor = () => this.color;
-    const getPlyerWins = () => this.wins;
-
-    return {getPlyerName, getPlyerType, getPlyerSymbol,
-        getPlyerColor, getPlyerWins};
+    return {
+        getPlyerName, getPlyerType, getPlyerSymbol,
+        getPlyerColor, getPlyerWins, countWins
+    };
 }
 
+const playBoard = (() => {
+    const writePlay = (player, symbol, color, boardSquare) => {
+        boardSquare.textContent = symbol;
+        boardSquare.style("color") = color;
+        //matrix = player at position
+    }
+    return { writePlay };
+})();
+
+const board = document.getElementsByClassName("boardSquare");
+/*
+Array.from(board).forEach(element => {
+    element.addEventListener("click",)
+});*/
+
+const lockPlayer = document.getElementsByClassName("lockPlayer");
+
+Array.from(lockPlayer).forEach(element => {
+    element.addEventListener("click", (e) => {
+        if (e.target.textContent == "Lock-in") {
+            let playerData = new FormData(element.parentElement);
+            playerFactory(playerData[0], playerData[1], playerData[2], playerData[3]);
+            e.target.textContent = "Unlock";
+            Array.from(element.parentElement.getElementsByTagName("input")).forEach(element => {
+                element.disabled = true;
+            });
+            e.target.disabled = false;
+        } else{
+            e.target.textContent = "Lock-in";
+            Array.from(element.parentElement.getElementsByTagName("input")).forEach(element => {
+                element.disabled = false;
+            });
+        }
+    })
+});
